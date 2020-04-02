062f08bf97b10cd42744840cb890b82bdac72a03
alex-byi <alex-byi@mail.ru>
2020-04-02 10:17:27 +0300

    remove VPP dependencies

--- cayenne-ant/src/main/java/org/apache/cayenne/tools/CayenneGeneratorTask.java
+++ cayenne-ant/src/main/java/org/apache/cayenne/tools/CayenneGeneratorTask.java
@@ -20,7 +20,6 @@ package org.apache.cayenne.tools;
 
 import java.io.File;
 
-import foundrylogic.vpp.VPPConfig;
 import org.apache.cayenne.configuration.xml.DataChannelMetaData;
 import org.apache.cayenne.dbsync.filter.NamePatternMatcher;
 import org.apache.cayenne.dbsync.reverse.configuration.ToolsModule;
@@ -33,7 +32,6 @@ import org.apache.cayenne.gen.ClientClassGenerationAction;
 import org.apache.cayenne.map.DataMap;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.types.Path;
-import org.apache.velocity.VelocityContext;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -51,7 +49,6 @@ public class CayenneGeneratorTask extends CayenneTask {
      * @since 4.1
      */
     protected String excludeEmbeddablesPattern;
-    protected VPPConfig vppConfig;
 
     protected File map;
     protected File additionalMaps[];
@@ -89,11 +86,6 @@ public class CayenneGeneratorTask extends CayenneTask {
     protected Boolean createpkproperties;
 
     public CayenneGeneratorTask() {
-    }
-
-    protected VelocityContext getVppContext() {
-        initializeVppConfig();
-        return vppConfig.getVelocityContext();
     }
 
     /**
@@ -246,7 +238,7 @@ public class CayenneGeneratorTask extends CayenneTask {
      * @param additionalMapsPath The additional DataMaps to set
      */
     public void setAdditionalMaps(Path additionalMapsPath) {
-        String additionalMapFilenames[] = additionalMapsPath.list();
+        String[] additionalMapFilenames = additionalMapsPath.list();
         this.additionalMaps = new File[additionalMapFilenames.length];
 
         for (int i = 0; i < additionalMapFilenames.length; i++) {
@@ -393,21 +385,4 @@ public class CayenneGeneratorTask extends CayenneTask {
         this.force = force;
     }
 
-    /**
-     * Provides a <code>VPPConfig</code> object to configure. (Written with createConfig()
-     * instead of addConfig() to avoid run-time dependency on VPP).
-     */
-    public Object createConfig() {
-        this.vppConfig = new VPPConfig();
-        return this.vppConfig;
-    }
-
-    /**
-     * If no VppConfig element specified, use the default one.
-     */
-    private void initializeVppConfig() {
-        if (vppConfig == null) {
-            vppConfig = VPPConfig.getDefaultConfig(getProject());
-        }
-    }
 }
